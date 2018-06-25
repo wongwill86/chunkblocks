5df5e68f1a4be0c4c01c42d76c04b459edf6ee91
wongwill86 <wongwill86@gmail.com>
2018-06-25 17:14:09 -0400

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = u'chunkblocks'
 year = '2018'
 author = u'Will Wong'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'0.1.0'
+version = release = u'version = release = '0.1.1''
 
 pygments_style = 'trac'
 templates_path = ['.']
